5a07c6097e3833a6c1321601be4f9992ba7a42a3
yamaneko05 <o.yamaneko0331@gmail.com>
2024-06-04 06:18:21 +0900

    first commit

---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,29 @@
+import { prettyDate } from "@/lib/utils";
+import Markdown from "react-markdown";
+
+async function getPost(id: string) {
+  return fetch(`http://localhost:1337/api/posts/${id}?populate=*`, { cache: "no-cache" })
+  .then(res => res.json())
+  .then(json => json.data);
+}
+
+export default async function Page({ params }: {
+  params: { id: string }
+}) {
+  const post = await getPost(params.id)
+
+  return (
+    <div className="p-4 w-full max-w-[720px] mx-auto">
+      <div className="text-2xl font-bold mb-2">{post.attributes.title}</div>
+      <div className="mb-2">{prettyDate(post.attributes.createdAt)}</div>
+      <img
+        src={`http://localhost:1337`+post.attributes.eyecatch.data.attributes.url}
+        alt=""
+        className="rounded-3xl border shadow-lg mb-6"
+      />
+      <div className="prose">
+        <Markdown>{post.attributes.content}</Markdown>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/posts/page.tsx
@@ -0,0 +1,34 @@
+import { BACKEND_URL } from "@/constants";
+import { prettyDate } from "@/lib/utils";
+import Link from "next/link";
+
+async function getPosts() {
+  return fetch("http://localhost:1337/api/posts?populate=*")
+  .then(res => res.json())
+  .then(json => json.data);
+}
+
+export default async function Page() {
+  const posts = await getPosts()
+
+  return (
+    <div className="p-4 w-full max-w-[720px] mx-auto">
+      <div className="text-xl sm:text-2xl font-bold mb-4 sm:mb-6">記事一覧</div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-6">
+        {[...posts, ...posts, ...posts].map((post: any) => (
+          <div className="col-span-1" key={post.id}>
+            <Link href={`/posts/${post.id}`}>
+              <div className="sm:text-xl font-bold">{post.attributes.title}</div>
+              <div className="text-xs sm:text-sm mb-2">{prettyDate(post.attributes.createdAt)}</div>
+              <img
+                src={BACKEND_URL+post.attributes.eyecatch.data.attributes.url}
+                alt=""
+                className="rounded-2xl border shadow-lg"
+              />
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
